0c79d61a79835a61bd9aa4bbe88a51df10d48d08
abghifareihand <info.abghifareihan@gmail.com>
2023-07-03 16:53:05 +0700

    feat: :rocket: add task 11

---
+++ 11_React Fundamental/praktikum/todo-app/src/App.js
@@ -0,0 +1,11 @@
+import Todos from "./pages/todos";
+
+function App() {
+  return (
+    <>
+      <Todos />
+    </>
+  );
+}
+
+export default App;
---
+++ 11_React Fundamental/praktikum/todo-app/src/mockDatas.js
@@ -0,0 +1,42 @@
+export const mockTodos = [
+  {
+    id: 1,
+    todo: "Introduction React",
+    completed: false,
+  },
+  {
+    id: 2,
+    todo: "React Fundamental",
+    completed: true,
+  },
+  {
+    id: 3,
+    todo: "Event Handling",
+    completed: false,
+  },
+  {
+    id: 4,
+    todo: "React Hook",
+    completed: true,
+  },
+  {
+    id: 5,
+    todo: "React Routing",
+    completed: false,
+  },
+  {
+    id: 6,
+    todo: "React Form",
+    completed: true,
+  },
+  {
+    id: 7,
+    todo: "Global State Management",
+    completed: false,
+  },
+  {
+    id: 8,
+    todo: "Introduction Restfull API",
+    completed: true,
+  },
+];
