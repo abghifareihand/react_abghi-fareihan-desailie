4bbe791bb8cd87e6659242f801e7c7a578f9e747
abghifareihand <info.abghifareihan@gmail.com>
2023-07-06 11:42:50 +0700

    fix: :bug: update task 12

--- 12_Event Handling/praktikum/event-handling/src/components/TodoItem.js
+++ 12_Event Handling/praktikum/event-handling/src/components/TodoItem.js
@@ -1,28 +1,31 @@
 import React from "react";
 import "../style/TodoItem.css";
 
-function TodoItem({ todo, onCheckboxChange, onDelete }) {
-  const handleCheckboxClick = () => {
-    onCheckboxChange(todo.id);
+class TodoItem extends React.Component {
+  handleCheckboxClick = () => {
+    this.props.onCheckboxChange(this.props.todo.id);
   };
 
-  const handleDeleteClick = () => {
-    onDelete(todo.id);
+  handleDeleteClick = () => {
+    this.props.onDelete(this.props.todo.id);
   };
 
-  return (
-    <li>
-      <input
-        type="checkbox"
-        checked={todo.completed}
-        onChange={handleCheckboxClick}
-      />
-      <span style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
-        {todo.title}
-      </span>
-      <button onClick={handleDeleteClick}>Hapus</button>
-    </li>
-  );
+  render() {
+    const { todo } = this.props;
+    return (
+      <li>
+        <input
+          type="checkbox"
+          checked={todo.completed}
+          onChange={this.handleCheckboxClick}
+        />
+        <span style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
+          {todo.title}
+        </span>
+        <button onClick={this.handleDeleteClick}>Hapus</button>
+      </li>
+    );
+  }
 }
 
 export default TodoItem;
--- 12_Event Handling/praktikum/event-handling/src/components/TodoList.js
+++ 12_Event Handling/praktikum/event-handling/src/components/TodoList.js
@@ -1,64 +1,79 @@
-import React, { useState } from "react";
+import React from "react";
 import TodoItem from "./TodoItem";
 import "../style/TodoList.css";
 
-function TodoList() {
-  const [inputValue, setInputValue] = useState("");
-  const [todos, setTodos] = useState([]);
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: "",
+      todos: [],
+    };
+  }
 
-  const handleInputChange = (event) => {
-    setInputValue(event.target.value);
+  handleInputChange = (event) => {
+    this.setState({ inputValue: event.target.value });
   };
 
-  const handleSubmit = (event) => {
+  handleSubmit = (event) => {
     event.preventDefault();
+    const { inputValue, todos } = this.state;
     if (inputValue.trim() === "") {
       alert("Mohon isi pekerjaan terlebih dahulu!");
       return;
     }
-    setTodos([...todos, { id: Date.now(), title: inputValue, completed: false }]);
-    setInputValue("");
+    const newTodo = {
+      id: Date.now(),
+      title: inputValue,
+      completed: false,
+    };
+    this.setState({ todos: [...todos, newTodo], inputValue: "" });
   };
 
-  const handleCheckboxChange = (todoId) => {
+  handleCheckboxChange = (todoId) => {
+    const { todos } = this.state;
     const updatedTodos = todos.map((todo) => {
       if (todo.id === todoId) {
         return { ...todo, completed: !todo.completed };
       }
       return todo;
     });
-    setTodos(updatedTodos);
+    this.setState({ todos: updatedTodos });
   };
 
-  const handleDelete = (todoId) => {
+  handleDelete = (todoId) => {
+    const { todos } = this.state;
     const updatedTodos = todos.filter((todo) => todo.id !== todoId);
-    setTodos(updatedTodos);
+    this.setState({ todos: updatedTodos });
   };
 
-  return (
-    <div className="todo-list">
-      <h2>Aplikasi Todo List</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={inputValue}
-          onChange={handleInputChange}
-          placeholder="Add todo..."
-        />
-        <button type="submit">Tambah</button>
-      </form>
-      <ul>
-        {todos.map((todo) => (
-          <TodoItem
-            key={todo.id}
-            todo={todo}
-            onCheckboxChange={handleCheckboxChange}
-            onDelete={handleDelete}
+  render() {
+    const { inputValue, todos } = this.state;
+    return (
+      <div className="todo-list">
+        <h2>Aplikasi Todo List</h2>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={inputValue}
+            onChange={this.handleInputChange}
+            placeholder="Add todo..."
           />
-        ))}
-      </ul>
-    </div>
-  );
+          <button type="submit">Tambah</button>
+        </form>
+        <ul>
+          {todos.map((todo) => (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              onCheckboxChange={this.handleCheckboxChange}
+              onDelete={this.handleDelete}
+            />
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default TodoList;
